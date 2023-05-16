6ff4c82496c3c969f433499f87c49154d2720db4
michaelhelvig <michaelycoco@hotmail.com>
2023-05-15 23:23:33 -0300

    upload

---
+++ src/test/java/com/portfolio/michaelhelvig/MichaelhelvigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.michaelhelvig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichaelhelvigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
